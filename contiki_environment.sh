77266db4987e0d790c0b813d62873e24f64b3c9d
Marlon W. Santos <marlon.santos.santos@icen.ufpa.br>
2019-12-19 20:33:28 -0300

    Adicionado contiki_environment.sh

---
+++ contiki_environment.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+#
+#
+#
+#		Contiki Environment prepare the local environment to run contiki 
+#		Copyright (C) 2019 Marlon W. Santos
+#
+#
+#	
+#   This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+
+path_ListEvents="./tools/cooja/java/org/contikios/cooja/dialogs/Events/ListEvents.java"
+
+  #Busca o path dentro do arquivo ListEvents.java
+function path_on_file(){
+  file_path=`grep -o /.*contiki $path_ListEvents`
+}
+
+	#Busca o caminho do diretório local
+function find_local_path(){
+  local_path=`pwd`
+}
+
+	#Muda o path do arquivo List Events pelo diretório atual
+function change_path(){
+  sed -i "s;$file_path;$local_path;g" $path_ListEvents
+}
+
+path_on_file
+find_local_path
+change_path
